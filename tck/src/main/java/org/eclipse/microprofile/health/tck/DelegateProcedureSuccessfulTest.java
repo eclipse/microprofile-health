87b8d4dcea991436a8982f363137c87b4c6445ef
Heiko Braun <ike.braun@googlemail.com>
2017-08-18 11:57:19 +0200

    Added a test to verify delegation across injection targets works

---
+++ tck/src/main/java/org/eclipse/microprofile/health/tck/DelegateProcedureSuccessfulTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2017 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICES file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.eclipse.microprofile.health.tck;
+
+import static org.eclipse.microprofile.health.tck.JsonUtils.asJsonObject;
+import static org.eclipse.microprofile.health.tck.TCKConfiguration.getHealthURL;
+
+import org.eclipse.microprofile.health.tck.deployment.DelegateCheck;
+import org.eclipse.microprofile.health.tck.deployment.DelegationTarget;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.StringReader;
+
+/**
+ * @author Heiko Braun
+ */
+@RunWith(Arquillian.class)
+public class DelegateProcedureSuccessfulTest extends SimpleHttp {
+
+    @Deployment
+    public static Archive getDeployment() throws Exception {
+        WebArchive deployment = ShrinkWrap.create(WebArchive.class, "tck.war");
+        deployment.addClass(DelegateCheck.class);
+        deployment.addClass(DelegationTarget.class);
+        return deployment;
+    }
+
+    /**
+     * Verifies CDI scoped beans can be used as delegate to resolve the system state
+     */
+    @Test
+    @RunAsClient
+    public void testSuccessfulDelegateInvocation() throws Exception {
+        Response response = getUrlContents(getHealthURL());
+
+        // status code
+        Assert.assertEquals(200, response.getStatus());
+
+        JsonReader jsonReader = Json.createReader(new StringReader(response.getBody().get()));
+        JsonObject json = jsonReader.readObject();
+
+        // response size
+        JsonArray checks = json.getJsonArray("checks");
+        Assert.assertEquals("Expected a single check response", 1, checks.size());
+
+
+        // single procedure response
+        Assert.assertEquals(
+                "Expected a dependent CDI bean to be invoked, to resolve the system state",
+                "delegate-check",
+                asJsonObject(checks.get(0)).getString("id")
+        );
+
+        Assert.assertEquals(
+                "Expected a successful check result",
+                "UP",
+                asJsonObject(checks.get(0)).getString("result")
+        );
+
+        // overall outcome
+        Assert.assertEquals(
+                "Expected overall outcome to be successful",
+                "UP",
+                json.getString("outcome")
+        );
+    }
+}
+
