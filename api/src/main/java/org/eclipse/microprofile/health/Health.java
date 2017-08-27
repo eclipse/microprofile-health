cc332e2d4be7614d2d1cfc85ec73180c040cfa87
John Ament <john.d.ament@gmail.com>
2017-08-27 02:59:43 -0400

    Introduce @Health as an actual qualifier (#70)
    In order to be a valid qualifier (#53) and allow look up, we need it to not specify a target, or a more broad selection of targets.

--- api/src/main/java/org/eclipse/microprofile/health/Health.java
+++ api/src/main/java/org/eclipse/microprofile/health/Health.java
@@ -22,16 +22,15 @@
 package org.eclipse.microprofile.health;
 
 import javax.inject.Qualifier;
-import java.lang.annotation.ElementType;
+import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 /**
  * Created by hbraun on 24.08.17.
  */
 @Qualifier
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.TYPE})
 public @interface Health {
 }
