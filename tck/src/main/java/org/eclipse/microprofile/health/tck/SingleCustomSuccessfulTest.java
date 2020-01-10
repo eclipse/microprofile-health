7e541b5068d6d0e7b39e308a35364fb94453f26e
Antoine Sabot-Durand <antoine@sabot-durand.net>
2020-01-10 10:20:20 +0100

    microprofile-health-81 Ability to execute only some health checks Adding annotations Amend specification Add TCK tests
    Signed-off-by: Antoine Sabot-Durand <antoine@sabot-durand.net>

---
+++ tck/src/main/java/org/eclipse/microprofile/health/tck/SingleCustomSuccessfulTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2017-2020 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICES file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.eclipse.microprofile.health.tck;
+
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+
+import org.eclipse.microprofile.health.tck.deployment.SuccessfulCustom;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.shrinkwrap.api.Archive;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.eclipse.microprofile.health.tck.DeploymentUtils.createWarFileWithClasses;
+
+/**
+ * @author Antoine Sabot-Durand
+ */
+public class SingleCustomSuccessfulTest extends TCKBase {
+
+    @Deployment
+    public static Archive getDeployment() {
+        return createWarFileWithClasses(SingleCustomSuccessfulTest.class.getSimpleName(), SuccessfulCustom.class);
+    }
+
+    /**
+     * Verifies the health integration with CDI at the scope of a server runtime
+     */
+    @Test
+    @RunAsClient
+    public void testSuccessResponsePayload() {
+        Response response = getUrlCustomHealthContents("goup1");
+
+        // status code
+        Assert.assertEquals(response.getStatus(),200);
+
+        JsonObject json = readJson(response);
+
+        // response size
+        JsonArray checks = json.getJsonArray("checks");
+        Assert.assertEquals(checks.size(),1,"Expected a single check response");
+
+        // single procedure response
+        assertSuccessfulCheck(checks.getJsonObject(0), "successful-check");
+
+        assertOverallSuccess(json);
+    }
+}
+
