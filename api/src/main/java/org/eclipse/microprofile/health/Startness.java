5c8dcfd416683205434fecbddea0610a40f691fc
xstefank <xstefank122@gmail.com>
2021-05-03 10:32:13 +0200

    Update OSGi version to 2.1

--- api/src/main/java/org/eclipse/microprofile/health/Startness.java
+++ api/src/main/java/org/eclipse/microprofile/health/Startness.java
@@ -21,17 +21,18 @@
  */
 package org.eclipse.microprofile.health;
 
-import javax.enterprise.util.AnnotationLiteral;
-import javax.inject.Qualifier;
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.enterprise.util.AnnotationLiteral;
+import javax.inject.Qualifier;
 
 /**
  *
@@ -40,7 +41,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * @author Martin Stefanko
  * @since 3.1
  */
-@Target({ TYPE, METHOD, PARAMETER, FIELD })
+@Target({TYPE, METHOD, PARAMETER, FIELD})
 @Retention(RUNTIME)
 @Documented
 @Qualifier
